6d6ce59741b077e5d258460190e5c71204bbf40c
Sven Baumann <baumann.sv@gmail.com>
2018-03-10 01:30:40 +0100

    Update php doc and add the right license

--- src/ZipArchiveCto.php
+++ src/ZipArchiveCto.php
@@ -1,14 +1,24 @@
 <?php
 
 /**
- * Contao Open Source CMS
+ * This file is part of menatwork/ziparchivecto.
  *
- * @copyright  MEN AT WORK 2013
- * @package    ZipArchiveCto
- * @license    GNU/LGPL
+ * (c) 2014-2018 MEN AT WORK.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    menatwork/ziparchivecto
+ * @author     Stefan Heimes <stefan_heimes@hotmail.com>
+ * @author     Andreas Isaak <andy.jared@googlemail.com>
+ * @author     David Maack <david.maack@arcor.de>
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  2014-2018 MEN AT WORK.
+ * @license    https://github.com/menatwork/syncCto/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
-
 namespace MenAtWork\ZipArchiveContaoComponent;
 
 /**
